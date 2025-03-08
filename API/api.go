db930b482ae0522d001f83b366ac72ab31c8fe45
GergesHany <gergeshany179@gmail.com>
2025-03-08 06:03:09 +0200

    API

---
+++ API/api.go
@@ -0,0 +1,54 @@
+package API
+
+import (
+	"GitHubEvent/modules"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func GetUrl(username string) string {
+	return fmt.Sprintf("https://api.github.com/users/%s/events", username)
+}
+
+func ParseJSONResponse(resp *http.Response) ([]modules.Event, error) {
+	var events []modules.Event
+	err := json.NewDecoder(resp.Body).Decode(&events)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse response: %v", err)
+	}
+	return events, nil
+}
+
+func HandleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func FetchGitHubActivity(username string) ([]modules.Event, error) {
+	url := GetUrl(username)
+
+	client := http.Client{
+		Timeout: time.Second * 5, // Timeout after 5 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Add("User-Agent", "GitHub-Activity")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	events, err := ParseJSONResponse(resp)
+	return events, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"GitHubEvent/API"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	
+	username := "Gergeshany"
+	events, err := API.FetchGitHubActivity(username)
+
+	if err != nil {
+		fmt.Printf("Error fetching GitHub activity: %v\n", err)
+        return
+	}
+
+	for _, event := range events {
+		fmt.Printf("%s - %s: %s\n", event.CreatedAt.Format(time.RFC1123Z), event.Type, event.Repo.Name)
+        if event.Payload.Issue.Number != 0 {
+            fmt.Printf("\tIssue: %d - %s\n", event.Payload.Issue.Number, event.Payload.Issue.Title)
+        } else if event.Payload.PullRequest.Number != 0 {
+            fmt.Printf("\tPull Request: %d - %s\n", event.Payload.PullRequest.Number, event.Payload.PullRequest.Title)
+        }
+    }
 }
